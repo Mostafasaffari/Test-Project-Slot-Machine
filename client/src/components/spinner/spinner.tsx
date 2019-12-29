5c2caf8a25ca953a5e52d4ac62b66d75a700b87e
Mostafa Saffari <mostafasaffari@gmail.com>
2019-12-29 18:02:42 +0330

    chore: we don't need interval pass to spinner

--- client/src/components/spinner/spinner.tsx
+++ client/src/components/spinner/spinner.tsx
@@ -2,12 +2,10 @@ import React from "react";
 import SpinnerWrapper from "./spinner.style";
 
 interface IProps {
-  intervalSpin: number;
   positionReel: number;
   randomPosition: number;
 }
 const Spinner: React.FC<IProps> = ({
-  intervalSpin,
   positionReel,
   randomPosition
 }) => {
@@ -16,9 +14,7 @@ const Spinner: React.FC<IProps> = ({
       <div
         className={[
           "spinner__spin--icon",
-          intervalSpin === 0 && positionReel === -1
-            ? "spinner__spin--question"
-            : ""
+          positionReel === -1 ? "spinner__spin--question" : ""
         ].join(" ")}
         style={{ backgroundPositionY: `${randomPosition || positionReel}px` }}
       ></div>
--- client/src/pages/app/slotMachine/SlotMachine.tsx
+++ client/src/pages/app/slotMachine/SlotMachine.tsx
@@ -19,7 +19,6 @@ const SlotMachine: React.FC = () => {
   const [positionReel1, setPositionReel1] = useState<number>(-1);
   const [positionReel2, setPositionReel2] = useState<number>(-1);
   const [positionReel3, setPositionReel3] = useState<number>(-1);
-  const [intervalSpin, setIntervalSpin] = useState<number>(0);
   const { t } = useTranslation();
   const dispatch = useDispatch();
 
@@ -29,14 +28,12 @@ const SlotMachine: React.FC = () => {
         const randomPosition = Math.floor(Math.random() * 10) * 64 * 2000;
         setPosition(randomPosition);
       }, 100);
-      setIntervalSpin(inter);
       const machineData: ISpin = await spinApi();
       setPositionReel1(calculatePositionOfIcon(machineData.Reel1));
       setPositionReel2(calculatePositionOfIcon(machineData.Reel2));
       setPositionReel3(calculatePositionOfIcon(machineData.Reel3));
       setTimeout(() => {
         clearInterval(inter);
-        setIntervalSpin(0);
         setPosition(0);
         dispatch(userActions.setUserCoin(machineData.coins));
       }, 2000);
@@ -50,21 +47,9 @@ const SlotMachine: React.FC = () => {
         <Button onClick={runSpin}>{t("slotMachine.spin")}</Button>
       </div>
       <div className="slotmachine__spin">
-        <Spinner
-          intervalSpin={intervalSpin}
-          positionReel={positionReel1}
-          randomPosition={position}
-        />
-        <Spinner
-          intervalSpin={intervalSpin}
-          positionReel={positionReel2}
-          randomPosition={position}
-        />
-        <Spinner
-          intervalSpin={intervalSpin}
-          positionReel={positionReel3}
-          randomPosition={position}
-        />
+        <Spinner positionReel={positionReel1} randomPosition={position} />
+        <Spinner positionReel={positionReel2} randomPosition={position} />
+        <Spinner positionReel={positionReel3} randomPosition={position} />
       </div>
     </SelotMachineWrapper>
   );
