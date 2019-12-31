68781dd9da135d0703c7115137c31a58a22e3fc9
mostafa saffari <mostafasaffari@gmail.com>
2019-12-31 11:30:45 +0330

    test: add a simple test for allcountries component

--- client/src/pages/app/allCountries/allCountries.test.tsx
+++ client/src/pages/app/allCountries/allCountries.test.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { ThemeProvider, DefaultTheme } from "styled-components";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
+import defaultTheme from "../../../style/themes/default";
 import AllCountries from "./";
 
-describe("App Component", () => {
-  it("Should render without crash", () => {});
+const renderComponent = (theme: DefaultTheme) =>
+  render(
+    <ThemeProvider theme={theme}>
+      <AllCountries />
+    </ThemeProvider>
+  );
+
+afterEach(cleanup);
+describe("All Countries Component", () => {
+  it("Should render without crash", () => {
+    const { asFragment } = renderComponent(defaultTheme);
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
