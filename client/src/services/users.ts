5edbef90508cc54e96ad19b3f508ee1f3829df8f
Mostafa Saffari <mostafasaffari@gmail.com>
2020-01-21 10:08:19 +0330

    refactor: user api with my api

--- client/src/services/users.ts
+++ client/src/services/users.ts
@@ -1,39 +1,34 @@
-import axios from "../helpers/apiHelper";
+import { MyAPI } from "../helpers/apiHelper";
 
 import { IUserRegister, IUserSignIn } from "../entities/user";
 
 import { localStore } from "../helpers/localStorage";
 import handleError from "../helpers/handleError";
 
+const api = new MyAPI();
+
 const registerApi = async (input: IUserRegister) => {
-  try {
-    const response = await axios.post(`/user/register`, {
-      ...input
-    });
-    return response.data.data;
-  } catch (err) {
-    return handleError(err);
-  }
+  const response = await api.post(`/user/register`, {
+    ...input
+  });
+  return response.data.data;
 };
 
 const signInApi = async (input: IUserSignIn) => {
-  try {
-    const response = await axios.post(`/user/signIn`, {
-      ...input
-    });
-    return response.data.data;
-  } catch (err) {
-    return handleError(err);
-  }
+  const response = await api.post(`/user/signIn`, {
+    ...input
+  });
+  return response.data.data;
 };
 
 const getUserInfoApi = async () => {
+  const apiWithAuth = new MyAPI({
+    headers: {
+      Authorization: localStore.get("token")
+    }
+  });
   try {
-    const response = await axios.post(`/user/getUserInfo`, null, {
-      headers: {
-        Authorization: localStore.get("token")
-      }
-    });
+    const response = await apiWithAuth.post(`/user/getUserInfo`);
     return response.data.data;
   } catch (err) {
     return handleError(err);
